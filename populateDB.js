9365cac76e7b798b69ce702ec833b174cee65ad6
mbdx4srt <35216966+mbdx4srt@users.noreply.github.com>
2021-03-31 15:21:48 +0100

    updated formatting

--- populateDB.js
+++ populateDB.js
@@ -19,57 +19,55 @@ async function write() {
         // console.log('Restaurant ' + xr)
         // console.log(restaurants[r])
         db.serialize(function () {
-            let stmt
-            try {
-                console.log(xr, restaurants[r].name, restaurants[r].image)
-                stmt = db.prepare("INSERT INTO restaurant (id, name, link) VALUES (?,?,?)"
-                    , xr, restaurants[r].name, restaurants[r].image);
-                stmt.run();
-            } finally {
-                stmt.finalize();
-            }}
-            )
-            xr = xr + 1
+                let stmt
+                try {
+                    console.log(xr, restaurants[r].name, restaurants[r].image)
+                    stmt = db.prepare("INSERT INTO restaurant (id, name, link) VALUES (?,?,?)"
+                        , xr, restaurants[r].name, restaurants[r].image);
+                    stmt.run();
+                } finally {
+                    stmt.finalize();
+                }
+            }
+        )
+        xr = xr + 1
 
-            for (const m in restaurants[r].menus) {
+        for (const m in restaurants[r].menus) {
+            db.serialize(function () {
+                let stmt
+                try {
+                    console.log(xm, restaurants[r].menus[m].title)
+                    stmt = db.prepare("INSERT INTO menu (id, title, restaurant_id) VALUES (?,?,?)",
+                        xm, restaurants[r].menus[m].title, xr);
+                    stmt.run();
+                } finally {
+                    stmt.finalize();
+                }
+            })
+            xm = xm + 1
+
+
+            for (const i in restaurants[r].menus[m].items) {
                 db.serialize(function () {
-                    let stmt
-                    try {
-                        console.log(xm, restaurants[r].menus[m].title)
-                        stmt = db.prepare("INSERT INTO menu (id, title, restaurant_id) VALUES (?,?,?)",
-                            xm, restaurants[r].menus[m].title, xr);
-                        stmt.run();
-                    } finally {
-                        stmt.finalize();
-                    }
-                })
-                xm = xm + 1
-
-
-                for (const i in restaurants[r].menus[m].items) {
-                    db.serialize(function () {
                         let stmt
-                    try {
-                        console.log(restaurants[r].menus[m].items[i])
-                        stmt = db.prepare("INSERT INTO menu_item (id, name, price, menu_id) VALUES (?,?,?,?)",
-                            xi, restaurants[r].menus[m].items[i].name,
-                            restaurants[r].menus[m].items[i].price, xm);
-                        stmt.run();
-                    } finally {
-                        stmt.finalize();
+                        try {
+                            console.log(restaurants[r].menus[m].items[i])
+                            stmt = db.prepare("INSERT INTO menu_item (id, name, price, menu_id) VALUES (?,?,?,?)",
+                                xi, restaurants[r].menus[m].items[i].name,
+                                restaurants[r].menus[m].items[i].price, xm);
+                            stmt.run();
+                        } finally {
+                            stmt.finalize();
+                        }
                     }
-                }
                 )
-                    xi = xi + 1
-                }
-
+                xi = xi + 1
             }
 
         }
+
     }
-
-
-
+}
 
 
 write()
